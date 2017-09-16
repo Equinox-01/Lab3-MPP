b82506e876a25a9070d80d71eafbe7c68bb3b9df
Equinox <sovereign.stas@yandex.ru>
2017-09-16 16:21:43 +0300

    Mutex foundation added

---
+++ Lab3_MPP/Lab3_MPP/Mutex.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_MPP
+{
+    public class Mutex
+    {
+        public void Lock()
+        {
+
+        }
+
+        public void Unlock()
+        {
+
+        }
+    }
+}
