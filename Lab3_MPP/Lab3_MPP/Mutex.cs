805d4c76c9c3124b1096dc1cb899f86e06b3df71
Equinox <sovereign.stas@yandex.ru>
2017-09-19 01:28:58 +0300

    Final Threads check added

--- Lab3_MPP/Lab3_MPP/Mutex.cs
+++ Lab3_MPP/Lab3_MPP/Mutex.cs
@@ -1,21 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading;
 
 namespace Lab3_MPP
 {
     public class Mutex
     {
+        private Thread lockingThread;
+
         public void Lock()
         {
-
+            while (Interlocked.CompareExchange(ref lockingThread, Thread.CurrentThread, null) != null)
+                Thread.Yield();
         }
 
         public void Unlock()
         {
-
+            Interlocked.Exchange(ref lockingThread, null);
         }
     }
 }
--- Lab3_MPP/Lab3_MPP/Program.cs
+++ Lab3_MPP/Lab3_MPP/Program.cs
@@ -1,16 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Lab3_MPP
 {
     class Program
     {
+        delegate void Func(int task_ident, ref int value_check);
+
+        const 
+            int THREAD_CAPACITY = 5,
+                ITERATION_CAPACITY = 2;
+
+        private static void Function(int task_ident, ref int value_check)
+        {
+            var mutex = new Mutex();
+            for (int j = 0; j < ITERATION_CAPACITY; j++)
+            {
+                mutex.Lock();
+                Console.WriteLine("Task {0}, value test variable: {1}", task_ident + 1, value_check++);
+                mutex.Unlock();
+                Thread.Sleep(300);
+            }
+        }
+
         static void Main(string[] args)
         {
-
+            var mutex = new Mutex();
+            int checkedValue = 0;
+            var threads = new Thread[THREAD_CAPACITY];
+            for (int i = 0; i < THREAD_CAPACITY; i++)
+            {
+                int numberOfTask = i;
+                threads[i] = new Thread(() =>
+                {
+                    Function(numberOfTask, ref checkedValue);
+                });
+                threads[i].Start();
+            }
+            Console.ReadLine();
         }
     }
 }
